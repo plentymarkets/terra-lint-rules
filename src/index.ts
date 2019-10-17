14f6d798268536f23877d144fa5dde2f18894c4a
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-10-17 11:43:30 +0200

    add index.ts and clean dist before the build

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { Rule as GetSetRule } from './rules/getSetRule';
+export { Rule as MaxDepthRule } from './rules/maxDepthRule';
+export { Rule as MaxFunctionLineCountRule } from './rules/maxFunctionLineCountRule';
